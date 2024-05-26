a1a3ca7f62c0d6c7a54daf8e4dadedc256d20dc7
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-05-26 14:39:42 +0200

    feat(users): add user model and fixtures

---
+++ src/Entity/Traits/DateTimeTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait DateTimeTrait
+{
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function autoSetCreatedAt(): static
+    {
+        if ($this->createdAt === null) {
+            $this->setCreatedAt(new \DateTimeImmutable());
+        }
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function autoSetUpdatedAt(): static
+    {
+        $this->setUpdatedAt(new \DateTimeImmutable());
+
+        return $this;
+    }
+}
