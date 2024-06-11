d07e35812ec420b9414711cc80b1cc34752a78e6
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-06-11 15:11:14 +0200

    feat(delivery): add crud for delivery

---
+++ src/Entity/Delivery/Delivery.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity\Delivery;
+
+use App\Entity\Traits\EnableTrait;
+use App\Repository\Delivery\DeliveryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: DeliveryRepository::class)]
+class Delivery
+{
+    use EnableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 255)]
+    #[Assert\NotBlank]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\Length(max: 1000)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Assert\PositiveOrZero]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
